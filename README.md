ea86c624f26f0924b1667f3aacd362a56baca714
Ron Ravid <ron64@post.com>
2015-04-07 21:05:37 +0900

    Added small economy window to demonstrate method for  power saving.

--- README.md
+++ README.md
@@ -4,5 +4,20 @@
 
 Example watchface showing the time, including seconds.
 
-This version also displays the battery level and status of the Bluetooth
-connection.
+This version also displays the battery level and status of the Bluetooth connection.
+
+## Economy window modified version by Ron
+
+This version demonstrate the concept of reducing power consumption of watch by adding a window for the part that update frequently. Thus reducing power consumption because:
+* reducing redrawing of non-updated part of the screen.
+* Reducing recalculating/rebuilding all other layers that are not updated frequently.
+
+### used steps
+1. Define `update_all()` as the method to be called by any function that updated part of the screen, such as update minutes, update battery etc. This method ensure the main window is shown.
+2. Define `update_only_sec()` to be called when only the frequent part is updated, such as update seconds. This method ensure the dedicated smaller window is shown, disabling draw of all other layer in main window. 
+3. Add calls to above functions.
+4. Add a window to be used only for the frequently updated part. This window root layer have update procedure that clear only the area used by the frequently updated part, and leaving the rest of main window visible.
+5. Separated the frequently updated data (seconds) to be in new window. It should overlap same part in main window. Alternatively, the layer can move between main window and small economy window according to which is active. 
+
+### Licence
+All modification I made are under MIT licence. (Original watchface is under whatever licence Pebble uses)
--- src/classio.c
+++ src/classio.c
@@ -5,6 +5,27 @@ static TextLayer *s_time_layer;
 static TextLayer *s_battery_layer;
 static TextLayer *s_connection_layer;
 
+static Window *s_sec_window;
+bool SecLoaded = false;
+static TextLayer *s_sec_layer;
+
+static void update_only_sec(){
+  if (!SecLoaded){
+    window_stack_push(s_sec_window, false);
+    SecLoaded=true;  
+    //optionally move frequently updated layer to frequently updated window.
+  }
+}
+static void update_all(){
+  if (SecLoaded){
+    window_stack_pop(false);
+    SecLoaded=false;
+    //optionally move frequently updated layer to main window
+  }
+}
+void sec_window_update(struct Layer *layer, GContext *ctx) {
+  graphics_fill_rect(ctx, GRect(88,40,40,34), 0, 0);
+}
 static void handle_battery(BatteryChargeState charge_state) {
   static char battery_text[] = "100% charged";
 
@@ -14,20 +35,30 @@ static void handle_battery(BatteryChargeState charge_state) {
     snprintf(battery_text, sizeof(battery_text), "%d%% charged", charge_state.charge_percent);
   }
   text_layer_set_text(s_battery_layer, battery_text);
+  update_all();
 }
 
 static void handle_second_tick(struct tm* tick_time, TimeUnits units_changed) {
   // Needs to be static because it's used by the system later.
   static char s_time_text[] = "00:00:00";
+  static char s_sec_text[] = "00";
 
   strftime(s_time_text, sizeof(s_time_text), "%T", tick_time);
   text_layer_set_text(s_time_layer, s_time_text);
+  strftime(s_sec_text, sizeof(s_sec_text), "%S", tick_time);
+  text_layer_set_text(s_sec_layer, s_sec_text);
 
-  handle_battery(battery_state_service_peek());
+  if ((units_changed & MINUTE_UNIT) >0){
+    handle_battery(battery_state_service_peek()); //don't know why it's needed at all
+    update_all();
+  }
+  else
+    update_only_sec();
 }
 
 static void handle_bluetooth(bool connected) {
   text_layer_set_text(s_connection_layer, connected ? "connected" : "disconnected");
+  update_all();
 }
 
 static void main_window_load(Window *window) {
@@ -58,9 +89,9 @@ static void main_window_load(Window *window) {
   // (This is why it's a good idea to have a separate routine to do the update itself.)
   time_t now = time(NULL);
   struct tm *current_time = localtime(&now);
-  handle_second_tick(current_time, SECOND_UNIT);
+  handle_second_tick(current_time, SECOND_UNIT+MINUTE_UNIT);
 
-  tick_timer_service_subscribe(SECOND_UNIT, handle_second_tick);
+  tick_timer_service_subscribe(SECOND_UNIT+MINUTE_UNIT, handle_second_tick);
   battery_state_service_subscribe(handle_battery);
   bluetooth_connection_service_subscribe(handle_bluetooth);
 
@@ -86,9 +117,26 @@ static void init() {
     .unload = main_window_unload,
   });
   window_stack_push(s_main_window, true);
+
+  // creating the smaller window for the frequent updates.
+  s_sec_window = window_create();
+  window_set_background_color(s_sec_window, GColorBlack);
+  Layer *window_sec_layer = window_get_root_layer(s_sec_window);
+  layer_set_update_proc(window_sec_layer, sec_window_update);
+
+  s_sec_layer = text_layer_create(GRect(88, 40, 40, 34));
+  text_layer_set_text_color(s_sec_layer, GColorWhite);
+  text_layer_set_background_color(s_sec_layer, GColorClear);
+  text_layer_set_font(s_sec_layer, fonts_get_system_font(FONT_KEY_GOTHIC_28_BOLD));
+  text_layer_set_text_alignment(s_sec_layer, GTextAlignmentLeft);
+  layer_add_child(window_sec_layer, text_layer_get_layer(s_sec_layer));
+
 }
 
 static void deinit() {
+  text_layer_destroy(s_sec_layer);
+  window_destroy(s_sec_window);
+
   window_destroy(s_main_window);
 }
 
